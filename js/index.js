1ccafc12dae3bf7280171ecfc909c6660dcf7d8a
VishwakarmaSaurabh <321saurabhv@gmail.com>
2020-07-16 00:31:41 +0530

    number precision bug fixed

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ window.onload = function (event) {
   const btn = document.getElementById("button");
 
   btn.addEventListener("click", () => {
-    result.innerHTML = eval((9 / 5) * temp.value + 32);
+    result.innerHTML = eval(((9 / 5) * temp.value + 32).toPrecision(3));
     temp.value = "";
   });
 
   document.addEventListener("keypress", (event) => {
     if (event.keyCode === 13) {
-      result.innerHTML = eval((9 / 5) * temp.value + 32);
+      result.innerHTML = eval(((9 / 5) * temp.value + 32).toPrecision(3));
       temp.value = "";
     }
   });
